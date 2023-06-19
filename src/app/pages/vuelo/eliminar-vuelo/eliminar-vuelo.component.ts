9a430908c271733fd66c3172052e0d8d3aa1d5b1
unknown <trujillofloresbrigit@gmail.com>
2023-06-19 12:21:15 -0500

    app

---
+++ src/app/pages/vuelo/eliminar-vuelo/eliminar-vuelo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-vuelo',
+  templateUrl: './eliminar-vuelo.component.html',
+  styleUrls: ['./eliminar-vuelo.component.css']
+})
+export class EliminarVueloComponent {
+
+}
